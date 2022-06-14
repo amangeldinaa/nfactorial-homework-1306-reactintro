23b1419f677ed0d3b24073fe2f32f2f4fedbff37
ARUZHAN AMANGELDINA <aruzhan.amangeldina@nu.edu.kz>
2022-06-14 18:37:45 +0600

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/custom.css';
+import { useMediaQuery } from "react-responsive";
+import imgOne from "./img/photo1.jpg";
+import imgTwo from "./img/photo2.jpg";
+import imgThree from "./img/photo3.jpg";
+import imgFour from "./img/photo4.jpg";
+import imgFive from "./img/photo5.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	const isDesktop = useMediaQuery({
+        query: "(min-width: 992px)"
+    });
+    const isMobile = useMediaQuery({
+        query: "(max-width: 480px)"
+    });
+	const text1 = <h3 style={{ color: 'white' }}>HEY</h3>;
+	const text2 = <h3 style={{ color: 'white' }}>LET'S</h3>;
+	const text3 = <h3 style={{ color: 'white' }}>GIVE</h3>;
+	const text4 = <h3 style={{ color: 'white' }}>ALL</h3>;
+	const text5 = <h3 style={{ color: 'white' }}>YOU CAN</h3>;
+ 	
+	return (
+     <div className="wrapper">
+		<div className={isDesktop ? "block" : "blockm"}>
+		  	<article
+      			className="article"
+      				style={{ backgroundImage: `url(${imgOne})` }}
+    		>{text1}
+    	  	</article>
+		</div>
+
+		<div className={isDesktop ? "block" : "blockm"}>
+			<article
+      			className="article"
+      				style={{ backgroundImage: `url(${imgTwo})` }}
+    		>{text2}
+    	 	</article>
+		</div>
+
+		<div className={isDesktop ? "block" : "blockm"}>
+			<article
+      			className="article"
+      			style={{ backgroundImage: `url(${imgThree})` }}
+    			>{text3}
+    	  	</article>
+		</div>
+
+		<div className={isDesktop ? "block" : "blockm"}>
+			<article
+      			className="article"
+      				style={{ backgroundImage: `url(${imgFour})` }}
+    		>{text4}
+    	  	</article>
+		</div>
+
+		<div className={isDesktop ? "block" : "blockm"}>
+			<article
+      			className="article"
+      				style={{ backgroundImage: `url(${imgFive})` }}
+    		>{text5}
+    	  	</article>
+		</div>		
+	</div>
   );
 }
+
 
 export default App;
